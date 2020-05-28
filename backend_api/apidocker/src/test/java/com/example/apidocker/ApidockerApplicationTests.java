371244adc3465241853fcdead29b82d18affe34f
Votre Nom <Vous@exemple.com>
2020-05-28 10:19:09 +0200

    first commit

---
+++ backend_api/apidocker/src/test/java/com/example/apidocker/ApidockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
